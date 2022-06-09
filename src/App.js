bcf9b4120ac552b38e68ba0f93eead7a06d182b8
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2022-06-09 04:12:44 +0200

    A11y v.0.1
    Working -> restart, upper font size, downer font size

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -10,16 +11,49 @@ import {
 } from 'react-router-dom';
 
 
+
 function App() {
+  const [fontSize, setFontSize] = React.useState(14);
+  const operation = (data) => {
+    // alert(data)
+    switch (data) {
+      case "upperFontSize": {
+        if (fontSize == 20) {
+          setFontSize(20)
+        } else
+          setFontSize(fontSize + 1)
+      }
+        break;
+      case "downerFontSize": {
+        if (fontSize == 9) {
+          setFontSize(9)
+        } else
+          setFontSize(fontSize - 1)
+      }
+        break;
+      case "restart": {
+        setFontSize(14)
+      }
+
+
+
+    }
+
+  }
   return (
+
     <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Accessibility />
-      <Footer />                
-    </Router>
+      <div style={{
+        fontSize: `${fontSize}px`
+      }}>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Accessibility func={operation} />
+        <Footer />
+      </div>
+    </Router >
   );
 }
 
--- src/components/Accessibility.js
+++ src/components/Accessibility.js
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faWheelchair,
-    faA,
-    faPlus,
-    faMinus
+    faEye,
+    faMagnifyingGlassPlus,
+    faMagnifyingGlassMinus,
+    faCircleHalfStroke,
+    faArrowRotateRight,
 } from '@fortawesome/free-solid-svg-icons';
 
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Accessibility.css';
 
 class Accessibility extends React.Component {
@@ -14,30 +16,40 @@ class Accessibility extends React.Component {
         super();
         this.state = {};
     }
+
+
+    handleClick = (operation) => {
+        this.props.func(operation);
+
+    }
+
     render() {
         return (
-            <header className="Accessibility">
+            <header className="Accessibility" >
                 <input type="checkbox" id="toggle" className="toggle"></input>
-                <nav className='Accessibility-nav'>
-                    <ul>
-                        {/* <li onClick={fontSize('upper')}>Powiększ tekst</li>
+                <div className='Accessibility-buttons'>
+                    {/* <li onClick={fontSize('upper')}>Powiększ tekst</li>
                     <li onClick={fontSize('downer')}>Pomniejsz tekst</li>
                     <li onClick={contrast('on')}>Wyższy kontrast</li>
                     <li onClick={contrast('negative')}>negative</li>
                     <li onClick={resizeTo()}>Resetuj</li> */}
-                        <li>
-                            <FontAwesomeIcon icon={faA} />
-                            <FontAwesomeIcon icon={faPlus} />
-                            Powiększ tekst
-                        </li>
-                        <li>
-                            <FontAwesomeIcon icon={faA} />
-                            <FontAwesomeIcon icon={faMinus} />
-                            Pomniejsz tekst
-                        </li>
-                    </ul>
+                    <button onClick={e => this.handleClick("upperFontSize")}>
+                        <FontAwesomeIcon icon={faMagnifyingGlassPlus} /> Powiększ tekst
+                    </button>
+                    <button onClick={e => this.handleClick("downerFontSize")}>
+                        <FontAwesomeIcon icon={faMagnifyingGlassMinus} /> Pomniejsz tekst
+                    </button>
+                    <button onClick={e => this.handleClick("higherContrast")}>
+                        <FontAwesomeIcon icon={faCircleHalfStroke} /> Wyższy kontrast
+                    </button>
+                    <button onClick={e => this.handleClick("negative")}>
+                        <FontAwesomeIcon icon={faEye} /> Negatyw
+                    </button>
+                    <button onClick={e => this.handleClick("restart")}>
+                        <FontAwesomeIcon icon={faArrowRotateRight} /> Restart
+                    </button>
 
-                </nav>
+                </div>
 
                 <label htmlFor="toggle" className='toggle-label'>
                     <span>
