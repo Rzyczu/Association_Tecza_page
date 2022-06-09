68231204aab29ca9c4c49262cf8d18100c431709
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2022-06-09 23:26:51 +0200

    Fix nav ul
    -Fixed nav links (to be in one ul in nav)
    -Pointer cursor added

--- src/components/old/Nav.js
+++ src/components/old/Nav.js
@@ -17,20 +17,18 @@ class Nav extends React.Component {
                     W Krakowie
                 </div>
                 <input type="checkbox" id="nav-toggle" className="nav-toggle"></input>
-                <nav className='nav-list'>
+                <nav>
                     <ul>
                         <li><a href="link" className='clr-1'>O nas</a></li>
                         <li><a href="link" className='clr-2'>Aktualności</a></li>
                         <li><a href="link" className='clr-3'>Nasze działania</a></li>
                         <li><a href="link" className='clr-5'>Jak możesz pomóc?</a></li>
                         <li><a href="link" className='clr-6'>Kontakt</a></li>
+                    </ul>
+                    <ul className='top-nav'>
                         <li><a href="link">Wpłać darowiznę</a></li>
                         <li><a href="link">1%</a></li>
                     </ul>
-                    {/* <ul className='top-nav'>
-                        <li><a href="link">Wpłać darowiznę</a></li>
-                        <li><a href="link">1%</a></li>
-                    </ul> */}
                 </nav>
 
                 <label htmlFor="nav-toggle" className='nav-toggle-label'>
