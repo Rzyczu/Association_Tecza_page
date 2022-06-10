b07295c22adcc329d4f5151fc4778a0be0c6442f
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2022-06-10 18:12:58 +0200

    News fixed
    Fixed component news on mobile devices

--- src/components/NewsElement.js
+++ src/components/NewsElement.js
@@ -17,13 +17,13 @@ class NewsElement extends React.Component {
 
     render() {
         return (
-            <div className={`News ${this.state.reverse ? "row-reverse" : "row"}`}            >
+            <div className={`News ${this.state.reverse ? "row-reverse" : "row"}`}>
                 <div className="News-photo pb-10"
                     style={this.state.reverse ? null : { marginRight: '3em' }}
                 >
                     <img src={this.props.img} alt={this.props.alt}></img>
                 </div>
-                <div class="content"
+                <div class="News-content"
                     style={this.state.reverse ? { marginRight: '3em' } : null}
                 >
                     <h3 className="pb-20 display-2">{this.props.title}</h3>
